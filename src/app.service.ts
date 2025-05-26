970c4b703aee3fc48763f92f2cabfb6bbb64b657
Leandro_Rodriguez_Valerio <lea_rodriguezvalerio@hotmail.com>
2025-05-26 19:35:55 -0300

    cambios varios

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@nestjs/common';
+export interface iTrack{
+id:number
+title:string
+duracion:number
+artist:string
+}
+export const tracks: iTrack[] =[
+{id:1,
+title:"cancion 1",
+duracion:120,
+artist:"interptrete1"
+},
+{id:2,
+title:"cancion 2",
+duracion:120,
+artist:"interptrete2"
+},
+{id:3,
+title:"cancion 3",
+duracion:120,
+artist:"interptrete3"
+}
+]
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello Mundo!';
+  getTrack(): iTrack[] {
+    return tracks;
   }
 }
